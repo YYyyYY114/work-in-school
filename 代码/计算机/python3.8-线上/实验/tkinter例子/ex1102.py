1285c589b3f365b1abbb391150244767dc4d388b
YYyyYY114 <150144712+YYyyYY114@users.noreply.github.com>
2023-11-26 22:36:33 +0800

    Add files via upload

---
+++ 代码/计算机/python3.8-线上/实验/tkinter例子/ex1102.py
@@ -0,0 +1,10 @@
+#ex1102.py
+from tkinter import *
+win=Tk()
+label = Label(win,text="Hello,Python")
+btn1= Button(win,text="click")
+label.pack()
+btn1.pack()
+win.title("Example11-2!!!!!!!!")     #title()方法
+win.geometry("600x400")     #geometry()方法
+win.mainloop()
